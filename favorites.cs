627a431f9bf742c78a0e16ea67e1d194a58f10e2
SOIP <38543876+sumofitsparts@users.noreply.github.com>
2018-10-23 12:38:31 -0400

    first

---
+++ favorites.cs
@@ -0,0 +1,9 @@
+public class Favorites
+{
+	public static void Main()
+	{
+	System.Console.Out.WriteLine("My name is Daniel\n");
+	System.Console.Out.WriteLine("My favorite movie is The Matrix\n");
+	System.Console.Out.WriteLine("My favorite color is blue");
+	}
+}
